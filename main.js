75670c41cbe685eb7f2ee37a62eb5f4d6fdc96c3
asyiraafnufail <asyiraaf4@gmail.com>
2025-05-14 02:17:59 +0700

    others

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    setTimeout(() => {
+      alert("🎉 Promo spesial! Daftar sekarang dan dapatkan diskon 50%!");
+    }, 1500);
+  };
+
+function kirimPesan() {
+    alert("📩 Pesan Anda terkirim!");
+  }
+  
+  
